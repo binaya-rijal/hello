0bfede89571e3b77761491069b23c5ce6bf6075b
Binaya-R <158580315+Binaya-R@users.noreply.github.com>
2024-02-15 07:22:40 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typing",{
+    strings:["Web Designer", "Web Developer", "Graphic Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
+function redirectToEmail() {
+    // Redirect the user to their email provider's website
+    window.location.href = "https://mail.google.com/";
+}
